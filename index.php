f84540dbdc3b258d4cc1836af2a96f306dc10fdc
mashuxa <mashuxa@tut.by>
2017-11-22 19:09:47 +0300

    first

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Animation_lb</title>
+    <link rel="stylesheet" href="css/grid.min.css">
+    <link rel="stylesheet" href="css/common.min.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+</head>
+
+<body class="grid">
+    <header class="header"> <a href="index.php" class="logo">Home</a> <span class="tagline">
+    <i>Animations library</i></span> </header>
+    <nav class="nav"> <a href="animate-css.html">Animate CSS</a> <a href="animate-jq.html">Animate JQuery</a> <a href="animate-cc.html">Animate CC</a>  </nav>
+    <main class="main">
+        <div class="ball-container">
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+        </div>
+    </main>
+    <footer class="footer"> 2017 © All rights reserved </footer> 
+</body>
+
+</html>
